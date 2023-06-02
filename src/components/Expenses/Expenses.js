4190e39164d900f71534aec4bda08c0388e0747a
eraybuyukkanat <erybykknt@gmail.com>
2023-06-02 22:57:22 +0300

    filtering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,8 +4,13 @@ import '../Expenses/Expenses.css';
 import Card from "../UI/Card";
 import ExpensesFilter from './ExpenseFilter';
 function Expenses(props){
-  const [dropdownData,setDropdownData] = useState("");
 
+
+ 
+  const [dropdownData,setDropdownData] = useState("");
+  const filteredExpenses = props.expenses.filter((expense)=>{
+    return expense.date.getFullYear().toString() === dropdownData;
+  });
     const getdropdownDataFunc = function(data){
       setDropdownData(data);
       console.log(dropdownData);
@@ -14,7 +19,7 @@ function Expenses(props){
     return <Card className="expenses">
      
         <ExpensesFilter selected={dropdownData} onChangeFilter={getdropdownDataFunc} />
-        {props.expenses.map((expense) => (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />))}
+        {filteredExpenses.map((expense) => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))}
     </Card>
 }
 
